e8b2182dc63580e8664f90898bbc6f8025bf503d
Shubham <shubhamshudhanshu@fynd-external.com>
2024-09-05 12:54:45 +0530

    rendered component implemented

--- src/components/settings/renderer.js
+++ src/components/settings/renderer.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import LevelSettings from "./levelSettings";
-import CommonLabel from "../common/label";
 import Tabs from "../common/tab";
+import { FormControl, MenuItem, Select } from "@mui/material";
+import CommonLabel from "../common/label";
 
 const levelConfig = {
   global: {
@@ -11,6 +12,11 @@ const levelConfig = {
   pages: {
     label: "Page Settings",
     childKey: "sections",
+    options: [
+      { label: "Home", id: "home" },
+      { label: "Cart Landing", id: "cart-landing" },
+      { label: "Cart Review", id: "cart-review" },
+    ],
   },
   sections: {
     label: "Section Settings",
@@ -23,26 +29,22 @@ const levelConfig = {
 };
 
 function Renderer({ object, level, path = [] }) {
-  const [activeTab, setActiveTab] = useState("parent");
-
+  const [activeTab, setActiveTab] = useState(0);
+  const [selectedChildIndex, setSelectedChildIndex] = useState(0);
   const currentLevelConfig = levelConfig[level] || {};
   const childKey = currentLevelConfig.childKey;
   const childLevelConfig = levelConfig[childKey] || {};
-
   const childItems = object[childKey] || [];
-
   const handleTabChange = (newTabIndex) => {
     setActiveTab(newTabIndex);
   };
 
-  console.log({ activeTab });
   return (
     <div>
-      <CommonLabel>{}</CommonLabel>
       <Tabs
         tabs={[
           {
-            label: currentLevelConfig.label,
+            label: "Settings",
             content: (
               <div>
                 <LevelSettings levelJson={object} path={path} />
@@ -54,12 +56,35 @@ function Renderer({ object, level, path = [] }) {
             label: childLevelConfig.label,
             content: (
               <div>
+                <FormControl
+                  fullWidth
+                  margin="normal"
+                  required
+                  className="px-4"
+                >
+                  <CommonLabel>Select {childKey}</CommonLabel>
+                  <Select
+                    labelId="tenant-select-label"
+                    name="User Type"
+                    value={"home"}
+                    onChange={(e) => {}}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    {childLevelConfig.options?.map((option) => {
+                      return (
+                        <MenuItem value={option.id}> {option.label}</MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
                 {childItems.map((childItem, index) => (
                   <Renderer
-                    key={`${level}-${index}`} // Add a key for React reconciliation
+                    key={`${level}-${index}`}
                     object={childItem}
                     level={childKey}
-                    path={[...path, childKey, index]} // Extend the path for the child
+                    path={[...path, childKey, index]}
                   />
                 ))}
               </div>
